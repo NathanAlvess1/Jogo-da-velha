c6acf686daea5b0511e52ee7b967c9d8699f8eb5
Nathan Alves <nathanalves2010@hotmail.com>
2021-05-31 15:30:41 -0300

    first commit

---
+++ JogodaVelha/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace jogodavelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.casa_1 = new System.Windows.Forms.Button();
+            this.casa_2 = new System.Windows.Forms.Button();
+            this.casa_3 = new System.Windows.Forms.Button();
+            this.casa_4 = new System.Windows.Forms.Button();
+            this.casa_5 = new System.Windows.Forms.Button();
+            this.casa_6 = new System.Windows.Forms.Button();
+            this.casa_7 = new System.Windows.Forms.Button();
+            this.casa_8 = new System.Windows.Forms.Button();
+            this.casa_9 = new System.Windows.Forms.Button();
+            this.botaoJogar = new System.Windows.Forms.Button();
+            this.botaoLimpar = new System.Windows.Forms.Button();
+            this.resultado = new System.Windows.Forms.Label();
+            this.Winner = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // casa_1
+            // 
+            this.casa_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.casa_1.Location = new System.Drawing.Point(41, 22);
+            this.casa_1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.casa_1.Name = "casa_1";
+            this.casa_1.Size = new System.Drawing.Size(79, 79);
+            this.casa_1.TabIndex = 0;
+            this.casa_1.UseVisualStyleBackColor = true;
+            this.casa_1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // casa_2
+            // 
+            this.casa_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.casa_2.Location = new System.Drawing.Point(128, 22);
+            this.casa_2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.casa_2.Name = "casa_2";
+            this.casa_2.Size = new System.Drawing.Size(79, 79);
+            this.casa_2.TabIndex = 1;
+            this.casa_2.UseVisualStyleBackColor = true;
+            this.casa_2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // casa_3
+            // 
+            this.casa_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.casa_3.Location = new System.Drawing.Point(215, 22);
+            this.casa_3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.casa_3.Name = "casa_3";
+            this.casa_3.Size = new System.Drawing.Size(78, 79);
+            this.casa_3.TabIndex = 2;
+            this.casa_3.UseVisualStyleBackColor = true;
+            this.casa_3.Click += new System.EventHandler(this.casa_3_Click);
+            // 
+            // casa_4
+            // 
+            this.casa_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.casa_4.Location = new System.Drawing.Point(41, 106);
+            this.casa_4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.casa_4.Name = "casa_4";
+            this.casa_4.Size = new System.Drawing.Size(79, 79);
+            this.casa_4.TabIndex = 3;
+            this.casa_4.UseVisualStyleBackColor = true;
+            this.casa_4.Click += new System.EventHandler(this.casa_4_Click);
+            // 
+            // casa_5
+            // 
+            this.casa_5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.casa_5.Location = new System.Drawing.Point(128, 107);
+            this.casa_5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.casa_5.Name = "casa_5";
+            this.casa_5.Size = new System.Drawing.Size(79, 78);
+            this.casa_5.TabIndex = 4;
+            this.casa_5.UseVisualStyleBackColor = true;
+            this.casa_5.Click += new System.EventHandler(this.casa_5_Click);
+            // 
+            // casa_6
+            // 
+            this.casa_6.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.casa_6.Location = new System.Drawing.Point(216, 107);
+            this.casa_6.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.casa_6.Name = "casa_6";
+            this.casa_6.Size = new System.Drawing.Size(77, 78);
+            this.casa_6.TabIndex = 5;
+            this.casa_6.UseVisualStyleBackColor = true;
+            this.casa_6.Click += new System.EventHandler(this.casa_6_Click);
+            // 
+            // casa_7
+            // 
+            this.casa_7.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.casa_7.Location = new System.Drawing.Point(41, 190);
+            this.casa_7.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.casa_7.Name = "casa_7";
+            this.casa_7.Size = new System.Drawing.Size(79, 78);
+            this.casa_7.TabIndex = 6;
+            this.casa_7.UseVisualStyleBackColor = true;
+            this.casa_7.Click += new System.EventHandler(this.casa_7_Click);
+            // 
+            // casa_8
+            // 
+            this.casa_8.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.casa_8.Location = new System.Drawing.Point(128, 191);
+            this.casa_8.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.casa_8.Name = "casa_8";
+            this.casa_8.Size = new System.Drawing.Size(79, 76);
+            this.casa_8.TabIndex = 7;
+            this.casa_8.UseVisualStyleBackColor = true;
+            this.casa_8.Click += new System.EventHandler(this.casa_8_Click);
+            // 
+            // casa_9
+            // 
+            this.casa_9.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.casa_9.Location = new System.Drawing.Point(215, 191);
+            this.casa_9.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.casa_9.Name = "casa_9";
+            this.casa_9.Size = new System.Drawing.Size(78, 76);
+            this.casa_9.TabIndex = 8;
+            this.casa_9.UseVisualStyleBackColor = true;
+            this.casa_9.Click += new System.EventHandler(this.casa_9_Click);
+            // 
+            // botaoJogar
+            // 
+            this.botaoJogar.Location = new System.Drawing.Point(74, 286);
+            this.botaoJogar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.botaoJogar.Name = "botaoJogar";
+            this.botaoJogar.Size = new System.Drawing.Size(184, 24);
+            this.botaoJogar.TabIndex = 9;
+            this.botaoJogar.Text = "JOGAR";
+            this.botaoJogar.UseVisualStyleBackColor = true;
+            this.botaoJogar.Click += new System.EventHandler(this.botaoJogar_Click);
+            // 
+            // botaoLimpar
+            // 
+            this.botaoLimpar.Location = new System.Drawing.Point(74, 316);
+            this.botaoLimpar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.botaoLimpar.Name = "botaoLimpar";
+            this.botaoLimpar.Size = new System.Drawing.Size(184, 24);
+            this.botaoLimpar.TabIndex = 10;
+            this.botaoLimpar.Text = "LIMPAR";
+            this.botaoLimpar.UseVisualStyleBackColor = true;
+            this.botaoLimpar.Click += new System.EventHandler(this.botaoLimpar_Click);
+            // 
+            // resultado
+            // 
+            this.resultado.AutoSize = true;
+            this.resultado.Location = new System.Drawing.Point(197, 457);
+            this.resultado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(0, 15);
+            this.resultado.TabIndex = 11;
+            // 
+            // Winner
+            // 
+            this.Winner.AutoSize = true;
+            this.Winner.Font = new System.Drawing.Font("Microsoft YaHei", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Winner.Location = new System.Drawing.Point(21, 483);
+            this.Winner.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Winner.Name = "Winner";
+            this.Winner.Size = new System.Drawing.Size(0, 42);
+            this.Winner.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(86, 343);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 25);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Click em jogar !!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 390);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Winner);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.botaoLimpar);
+            this.Controls.Add(this.botaoJogar);
+            this.Controls.Add(this.casa_9);
+            this.Controls.Add(this.casa_8);
+            this.Controls.Add(this.casa_7);
+            this.Controls.Add(this.casa_6);
+            this.Controls.Add(this.casa_5);
+            this.Controls.Add(this.casa_4);
+            this.Controls.Add(this.casa_3);
+            this.Controls.Add(this.casa_2);
+            this.Controls.Add(this.casa_1);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Jogo da Velha";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button casa_1;
+        private System.Windows.Forms.Button casa_2;
+        private System.Windows.Forms.Button casa_3;
+        private System.Windows.Forms.Button casa_4;
+        private System.Windows.Forms.Button casa_5;
+        private System.Windows.Forms.Button casa_6;
+        private System.Windows.Forms.Button casa_7;
+        private System.Windows.Forms.Button casa_8;
+        private System.Windows.Forms.Button casa_9;
+        private System.Windows.Forms.Button botaoJogar;
+        private System.Windows.Forms.Button botaoLimpar;
+        private System.Windows.Forms.Label resultado;
+        private System.Windows.Forms.Label Winner;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
+
+
---
+++ JogodaVelha/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jogodavelha
+{
+    public partial class Form1 : Form
+    {
+
+        string initialState = "X";
+
+        public Form1()
+        {
+            InitializeComponent();
+            casa_1.Enabled = false;
+            casa_2.Enabled = false;
+            casa_3.Enabled = false;
+            casa_4.Enabled = false;
+            casa_5.Enabled = false;
+            casa_6.Enabled = false;
+            casa_7.Enabled = false;
+            casa_8.Enabled = false;
+            casa_9.Enabled = false;
+        }
+
+        
+        //Usado para fazer a mudança de jogador usando uma condição if-else
+        private string SwitchPlayer()
+        {
+            if (initialState == "X")
+            {
+                initialState = "O";
+            }
+            else
+            {
+                initialState = "X";
+            }
+            return initialState;
+        }
+        //metodo usado para fazer o bloqueio das casas.
+        private void blockCasas()
+        {
+            casa_1.Enabled = false;
+            casa_2.Enabled = false;
+            casa_3.Enabled = false;
+            casa_4.Enabled = false;
+            casa_5.Enabled = false;
+            casa_6.Enabled = false;
+            casa_7.Enabled = false;
+            casa_8.Enabled = false;
+            casa_9.Enabled = false;
+
+        }
+
+        //metodo usado para verificação da vitoria usando if-else
+        private void verificyVitory()
+        {
+            blockCasas();
+
+            if (initialState == "O")
+            {
+                Winner.Text = "JOGADOR 1 VENCEU";
+            }
+            else
+            {
+                Winner.Text = "JOGADOR 2 VENCEU";
+            }
+
+            botaoLimpar.Enabled = false;
+        }
+        
+        //metodo usado para verificação da velha usando if
+        private void verificaVelha()
+        {
+            if (casa_1.Text != "-"
+                && casa_2.Text != "-"
+                && casa_3.Text != "-"
+                && casa_4.Text != "-"
+                && casa_5.Text != "-"
+                && casa_6.Text != "-"
+                && casa_7.Text != "-"
+                && casa_8.Text != "-"
+                && casa_9.Text != "-"
+                )
+            {
+                Winner.Text = "DEU VELHA";
+                label1.Text = "DEU VELHA";
+                blockCasas();
+                label1.Text = "Vitoria do "+initialState;
+                botaoLimpar.Enabled = false;
+
+            }
+        }
+
+        //metodo que vai ser chamado no botao limpar casas, setando texto inicial definido como -, pois deixando vazio nao funciona.
+        private void limpar_Casas()
+        {
+            casa_1.Text = "-";
+            casa_2.Text = "-";
+            casa_3.Text = "-";
+            casa_4.Text = "-";
+            casa_5.Text = "-";
+            casa_6.Text = "-";
+            casa_7.Text = "-";
+            casa_8.Text = "-";
+            casa_9.Text = "-";
+            initialState = "X";
+        }
+
+
+        private void botaoLimpar_Click(object sender, EventArgs e)
+        {
+            limpar_Casas();
+            label1.Text = "Jogo zerado Click em Jogar";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            casa_1.Text = SwitchPlayer();
+
+            if (casa_1.Text == casa_2.Text && casa_2.Text == casa_3.Text)
+            {
+                verificyVitory();
+            }
+            else if (casa_1.Text == casa_4.Text && casa_4.Text == casa_7.Text)
+            {
+                verificyVitory();
+            }
+            else if (casa_1.Text == casa_5.Text && casa_5.Text == casa_9.Text)
+            {
+                verificyVitory();
+            }
+            verificaVelha();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            casa_2.Text = SwitchPlayer();
+
+            if (casa_2.Text == casa_5.Text && casa_5.Text == casa_8.Text)
+            {
+                verificyVitory();
+            }
+            else if (casa_2.Text == casa_1.Text && casa_2.Text == casa_3.Text)
+            {
+                verificyVitory();
+            }
+            verificaVelha();
+
+        }
+
+        private void casa_3_Click(object sender, EventArgs e)
+        {
+            casa_3.Text = SwitchPlayer();
+            if (casa_3.Text == casa_2.Text && casa_3.Text == casa_1.Text)
+            {
+                verificyVitory();
+            }
+            else if (casa_3.Text == casa_5.Text && casa_5.Text == casa_7.Text)
+            {
+                verificyVitory();
+            }
+            else if (casa_3.Text == casa_6.Text && casa_6.Text == casa_9.Text)
+            {
+                verificyVitory();
+            }
+            verificaVelha();
+        }
+
+        private void casa_4_Click(object sender, EventArgs e)
+        {
+            casa_4.Text = SwitchPlayer();
+
+            if (casa_4.Text == casa_5.Text && casa_5.Text == casa_6.Text)
+            {
+                verificyVitory();
+            }
+            else if (casa_4.Text == casa_1.Text && casa_4.Text == casa_7.Text)
+            {
+                verificyVitory();
+            }
+            verificaVelha();
+        }
+
+        private void casa_5_Click(object sender, EventArgs e)
+        {
+            casa_5.Text = SwitchPlayer();
+            if (casa_5.Text == casa_1.Text && casa_5.Text == casa_9.Text)
+            {
+                verificyVitory();
+            }
+            else if (casa_5.Text == casa_3.Text && casa_5.Text == casa_7.Text)
+            {
+                verificyVitory();
+            }
+            else if (casa_5.Text == casa_4.Text && casa_5.Text == casa_6.Text)
+            {
+                verificyVitory();
+            }
+            else if (casa_5.Text == casa_2.Text && casa_5.Text == casa_8.Text)
+            {
+                verificyVitory();
+            }
+            verificaVelha();
+
+        }
+
+        private void casa_6_Click(object sender, EventArgs e)
+        {
+            casa_6.Text = SwitchPlayer();
+            if (casa_6.Text == casa_4.Text && casa_6.Text == casa_5.Text)
+            {
+                verificyVitory();
+            }
+            else if (casa_6.Text == casa_3.Text && casa_6.Text == casa_9.Text)
+            {
+                verificyVitory();
+            }
+            verificaVelha();
+
+        }
+
+        private void casa_7_Click(object sender, EventArgs e)
+        {
+            casa_7.Text = SwitchPlayer();
+            if (casa_7.Text == casa_4.Text && casa_4.Text == casa_1.Text)
+            {
+                verificyVitory();
+            }
+            else if (casa_7.Text == casa_5.Text && casa_5.Text == casa_3.Text)
+            {
+                verificyVitory();
+            }
+            else if (casa_7.Text == casa_8.Text && casa_8.Text == casa_9.Text)
+            {
+                verificyVitory();
+            }
+            verificaVelha();
+        }
+
+        private void casa_8_Click(object sender, EventArgs e)
+        {
+            casa_8.Text = SwitchPlayer();
+            if (casa_8.Text == casa_5.Text && casa_5.Text == casa_2.Text)
+            {
+                verificyVitory();
+            }
+            else if (casa_8.Text == casa_7.Text && casa_7.Text == casa_9.Text)
+            {
+                verificyVitory();
+            }
+            verificaVelha();
+        }
+
+        private void casa_9_Click(object sender, EventArgs e)
+        {
+            casa_9.Text = SwitchPlayer();
+            if (casa_9.Text == casa_8.Text && casa_8.Text == casa_7.Text)
+            {
+                verificyVitory();
+            }
+            else if (casa_9.Text == casa_5.Text && casa_5.Text == casa_1.Text)
+            {
+                verificyVitory();
+            }
+            else if (casa_9.Text == casa_6.Text && casa_6.Text == casa_3.Text)
+            {
+                verificyVitory();
+            }
+            verificaVelha();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botaoJogar_Click(object sender, EventArgs e)
+        {
+            casa_1.Enabled = true;
+            casa_2.Enabled = true;
+            casa_3.Enabled = true;
+            casa_4.Enabled = true;
+            casa_5.Enabled = true;
+            casa_6.Enabled = true;
+            casa_7.Enabled = true;
+            casa_8.Enabled = true;
+            casa_9.Enabled = true;
+            Winner.Text = "";
+            limpar_Casas();
+            label1.Text = "Jogando...";
+            botaoLimpar.Enabled = true;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
